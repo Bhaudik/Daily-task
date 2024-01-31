8fe1f5cd1a508b5dfa9fb1567aa30bae371c2511
Bhaudik <pumbhadiyabhaudik@gmail.com>
2024-01-31 15:21:51 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>daily task</title>
+</head>
+<body>
+    <center><h1>hello this is over daliy task app</h1></center>
+</body>
+</html>
